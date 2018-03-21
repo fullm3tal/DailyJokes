211b4f82b69159a4f1a12f1dbfb4bb4e551e5b9b
root <jayantfordreams@gmail.com>
2018-03-22 02:18:16 +0530

    splash commit

---
+++ app/src/main/java/com/example/root/dailyjokes/AppConstants.java
@@ -0,0 +1,13 @@
+package com.example.root.dailyjokes;
+
+/**
+ * Created by root on 22/3/18.
+ */
+
+public class AppConstants {
+
+    private AppConstants(){
+
+    }
+    public static final int SPLASH_TIMEOUT=3000;
+}
